05dd61591bbf3af6c24159adfd05a0e3f0b5479a
Zzebastian <sebastian.scheverin@gmail.com>
2024-10-10 05:50:26 -0300

    Local Storage con comentarios

---
+++ 02 JavaScript/01.03 ToDo app Local Storage/script.js
@@ -0,0 +1,153 @@
+﻿const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
+
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
+let currentTask = {};
+// De esta forma, si no hay información, va a actuar el "||" y generar un array en blanco
+
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+    // unshift es un método para agregar elementos al inicio de un array
+  } else {
+    taskData[dataArrIndex] = taskObj;
+  }
+
+  localStorage.setItem("data", JSON.stringify(taskData));
+  updateTaskContainer()
+  reset()
+};
+
+const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+      // { id, title, date, description } deconstruye taskData, transformando taskData.id en id etc
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
+        </div>
+      `
+    }
+  );
+};
+
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+  // Esta función no tiene "{}" porque de lo contrario tendría que además contener un "return"
+  // (item)=>{ return item.id === buttonEl.parentElement.id}
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
+  localStorage.setItem("data", JSON.stringify(myTaskArr));
+  // localStorage.setItem("data", myTaskArr);
+  // Step 55
+  // If you check the "Application" tab of your browser console, you'll notice a series of [object Object]. This is because everything you save in localStorage needs to be in string format.
+  // Hay una imagen guardada que muestra esto.
+
+  // JSON.stringify Es para el guardado de datos de forma útil
+  // JSON.parse Es para recuperar esos datos
+
+  //CODIGO ELIMINADO
+  /*const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+  ];
+  
+  localStorage.setItem("data", JSON.stringify(myTaskArr));
+  
+  localStorage.clear();
+  
+  const getTaskArr = localStorage.getItem("data")
+  console.log(getTaskArr)
+  
+  const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+  console.log(getTaskArrObj);*/
+}
+
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");
+}
+
+const reset = () => {
+  addOrUpdateTaskBtn.innerText = "Add Task";
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
+if (taskData.length) {
+  updateTaskContainer();
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+// classList.toggle() aplica o elimina la clase 'hiden' al elemento taskForm
+
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  // Se crea la constante porque es una buena práctica, que facilita entendimiento y futuras depuraciones.
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
+    confirmCloseDialog.showModal();
+    // este "Modal" se utiliza para alterar el elemento <dialog> de HTML, el cual solo se muestra siempre que se lo llame, y una vez ahí solo se va si se le solicita desde JS
+  } else {
+    reset();
+  }
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  reset()
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // e.preventDefault() es un método que evita el "refresco" de la pantalla cuando se envía el formulario
+  addOrUpdateTask();
+});
