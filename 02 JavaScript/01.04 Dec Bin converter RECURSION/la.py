1c9dd707cf68a816377f66334b82e9ad6926d190
Zzebastian <sebastian.scheverin@gmail.com>
2024-10-13 01:01:20 -0300

    Recursividad y Time delay

---
+++ 02 JavaScript/01.04 Dec Bin converter RECURSION/la.py
@@ -0,0 +1,11 @@
+﻿def count_down_and_up(number):
+    print(number)
+    
+    if number == 0:
+        print("Reached base case")
+        return
+    else:
+        count_down_and_up(number - 1)
+        print(number)
+
+count_down_and_up(3)
