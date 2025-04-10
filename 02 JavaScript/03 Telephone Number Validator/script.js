9177c23094b70b88f52ba2735e75a07a98de80bc
Zzebastian <sebastian.scheverin@gmail.com>
2025-04-10 03:48:22 -0300

    Completado, sin aprovación de FCC pero funcina

--- 02 JavaScript/03 Telephone Number Validator/script.js
+++ 02 JavaScript/03 Telephone Number Validator/script.js
@@ -1,9 +1,8 @@
 const clearBtn = document.getElementById("clear-btn");
 const checkBtn = document.getElementById("check-btn");
 const escribir = (msj,nro) => {
-  
-  document.querySelector("#results-div").textContent += msj + nro + "\n";
-  
+  document.querySelector("#results-div").textContent += msj + nro;
+  // document.querySelector("#results-div").innerHTML += `<p>${msj} + ${nro}<p>`;
 }
 const borrar = (event) => {
   event.preventDefault();
@@ -15,9 +14,9 @@ const testear = (event) => {
   const str = document.getElementById("user-input").value;
   
   
-
+  
   if (str =="") {alert("Please provide a phone number")}
-  // const regex = /(.?[0-9])?[ ]?[\-(]?([0-9]{3,})[-)]?[ ]?([0-9]{3,})[ \-]?([0-9]{4,})/;
+  
   const regex = /(.?[0-9])?[ ]?([\-(])?([0-9]{3,})([-)])?[ ]?([0-9]{3,})[ \-]?([0-9]{4,})/;
   const match = str.match(regex);
   if (!match){return escribir("Invalid US number: ",str)}
@@ -25,17 +24,13 @@ const testear = (event) => {
   
   
   if ((match.includes("(") && !match.includes(")"))||(!match.includes("(") && match.includes(")"))){
-    
     return escribir("Invalid US number: ",str)
   }
   
   const texto = match.join("").replace("(","").replace(")","");
-  
-  
-  
+
   if ((texto.replace(/\-/g,"").length == 11 && texto[0]==1) || texto.replace(/\-/g,"").length == 10) {return escribir("Valid US number: ",str)}
   else {return escribir("Invalid US number: ",str)}
-  
 }
 
 clearBtn.addEventListener("click", borrar);
